1505d8bc424139b541a8cb511ec0593e3d120115
ibnshayed <ibnshayed@gmail.com>
2022-08-04 16:28:37 +0600

    server and root package.json and server updated

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ io.on("connection", (socket) => {
 	});
 });
 
-const port = 3000
+const port = process.env.PORT || 3000
 httpServer.listen(port, () => {
 	console.log(`Server is connected to http://localhost:${port}`)
 });
